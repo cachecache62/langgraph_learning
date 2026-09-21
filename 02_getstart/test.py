0bfa31e2ec40511f51512144071f5ee82fc79d86
cachechache62 <2978737704@qq.com>
2026-09-21 15:50:31 +0800

    Add LangGraph learning project

---
+++ 02_getstart/test.py
@@ -0,0 +1,11 @@
+
+import sys
+import langgraph
+import pydantic
+import langchain_core
+
+print("Python 路径：", sys.executable)
+
+print("langgraph 路径：", langgraph.__file__)
+print("pydantic 路径：", pydantic.__file__)
+print("langchain_core 路径：", langchain_core.__file__)
